46af240b8b1aecc3e09365a52eadebec8318c65a
thehugoreis <hugoreis2919@gmail.com>
2019-04-04 10:19:24 -0300

    Adicao/atualizacao de comentario

--- ex10.java
+++ ex10.java
@@ -19,37 +19,41 @@ class ex10 {
 		int numJogadores = 1;
 		int checagem = menIni.checagem;
 
+		//inicia o jogo enquanto a variavel telaIni na classe menuInicial for igual a um.
 		while (menIni.telaIni == 1) {
 
 			if (menIni.menuInicial == 0) {
-				menIni.telaBoasvindas();
+				menIni.telaBoasvindas(); //Caso a variavel menuInicial for igual a zero eh mostrado na tela o menu inicial.
 			} else if (menIni.menuInicial == 1) {
-				menIni.desc();
+				menIni.desc(); //Caso a variavel menuInicial for igual a um eh apresentado a descricao do jogo.
 
 			} else if (menIni.menuInicial == 2) {
-				menIni.cjogar();
+				menIni.cjogar(); //Caso a variavel menuInicial for igual a dois instrucoes de como jogar sao mostradas.
 
 			} else if (menIni.menuInicial == 3) {
 
+				//Caso a variavel menuInicial for igual a tres o jogo comeca.
 				if (checagem == 1) {
 					System.out.println("\nNesta sessao jogaram [1] um jogador ou [2] dois jogadores?");
-					numJogadores = resp.nextInt();
+					numJogadores = resp.nextInt(); // Pergunta e registra numero de jogadores.
 
+					//Caso o numero de jogadores for igual a um o bloco de perguntas para um jogador inicia.
 					if (numJogadores == 1) {
 
-						while (checagem == 1) {
+						pgt.jgd1();//Metodo que pergunta e registra o nome/nick do jogador e define pontos igual a zero.
 
-							pgt.jgd1();
+						while (checagem == 1) {
 						
+							//Metodos referentes as perguntas e suas resposta corretas.
 							pgt.a1();
 							pgt.b1();
 							pgt.c1();
 
 							System.out.println("\nDeseja jogar novamente? [1 - sim]/[2 - nao]");
-							int respNjogador = resp.nextInt();
+							int respNjogador = resp.nextInt();//Registra decisao do jogador em jogar novamente ou nao.
 
 							if (respNjogador == 1) {
-								System.out.println("\nOk");
+								System.out.println("\n");//Caso o jogador deseje jogar novamente uma nova linha eh impressa e o loop de perguntas reinicia
 							} else if (respNjogador == 2) {
 								checagem = 0;
 								obj1.msg();
@@ -58,7 +62,6 @@ class ex10 {
 								System.out.println("\nOpcao invalida");
 								System.exit(0);
 							}
-
 
 						}
 
