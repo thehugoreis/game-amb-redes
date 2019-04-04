8f85e8c4b925d67e304153fb3bc058bb1078da2d
thehugoreis <hugoreis2919@gmail.com>
2019-04-04 20:39:09 -0300

    nome e pontos Jogador 2 - comentarios atualizados

--- Jogador2.java
+++ Jogador2.java
@@ -7,8 +7,9 @@ class Jogador2 {
 	String nome;
 	String sexo;
 	String idade;
-	int pontosJ2 = 0;
+	int pontosJ2 = 0;//Garada os pontos da rodade referentes ao jogador dois
 
+	//Metodo que pergunta o nome/nick do jogador 2
 	void nome() {
 	System.out.println("\nJogador 2: Qual o seu nome ou nickname?");
 	nome = resp.nextLine();
