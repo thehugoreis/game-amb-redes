67d1341248fdd2db3cf442791758608160a52243
thehugoreis <hugoreis2919@gmail.com>
2019-04-06 10:12:35 -0300

    tela inicial - comentarios atualizados

--- menuInicial.java
+++ menuInicial.java
@@ -11,6 +11,7 @@ class menuInicial {
 	int menuInicial = 0;
 	int telaIni = 1;
 
+	//Tela inicial do jogo. Apresenta opcoes para o jogador escolher. 
 	void telaBoasvindas() {
 		System.out.println("\nOla, voce inicou o jogo de perguntas e repostas.\nEscolha uma das opcoes a seguir:\n");
 		System.out.println("[1]-Descricao do jogo.");
@@ -25,21 +26,25 @@ class menuInicial {
 		}
 	}
 
+	//Tela onde e mostrado a descricao do jogo e seu proposito.
 	void desc() {
 		System.out.println("\nDescricao do jogo aqui!\n");
 		System.out.println("\n\nVoltar a tela inicial? [1-Sim]/[2-Nao]");
 		telaIni = resp.nextInt();
 
 	}
+	//Tela com descricoes de como jogar.
 	void cjogar() {
 		System.out.println("\nComo jogar aqui!");
 		System.out.println("\n\nVoltar a tela inicial? [1-Sim]/[2-Nao]");
 		telaIni = resp.nextInt();
 
 	}
+	//Resseta a variavel menuInicial. Fazendo o loop voltar ao metodo telaBoasvindas.
 	void resetIni() {
 		menuInicial = 0;
 	}
+	//Resset para a tela de escolha de jogadores.
 	void resetTojogador() {
 		menuInicial = 3;
 	}
