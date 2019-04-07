0db9a35e97252d2586e98b4998bb3684825b541d
thehugoreis <hugoreis2919@gmail.com>
2019-04-07 20:43:06 -0300

    Perguntas e mensagem final atualizadas

--- Mensagem.java
+++ Mensagem.java
@@ -2,6 +2,7 @@
 import java.util.Scanner;
 
 class Mensagem {
+	//Mensagem final do jogo
 	void msg(){
 		System.out.println("\nObrigado por jogar!\n\nCreditos:\n	Hugo reis\n	Antonio Carlos\n\n**Projeto de Ambiente de Redes - 2019**");
 	}
